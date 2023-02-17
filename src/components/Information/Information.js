e01e9240ede345e936a76b13032ee9e50b9797c0
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-02-17 12:01:43 +0200

    change style

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -17,74 +17,74 @@ const Information = ({weather}) => {
 
     return (
         <div className={'information'}>
-            <div className={'information-main'}>
-                <div className={'block'}>
-                    <p>Temperature min</p>
-                    <div>{weather.data.main.temp_min}<sup>&deg;C</sup></div>
-                </div>
-
-                <div className={'block'}>
-                    <p>Temperature max</p>
-                    <div>{weather.data.main.temp_max}<sup>&deg;C</sup></div>
-                </div>
-
-                <div className={'block'}>
-                    <p>Humidity</p>
-                    <div>{weather.data.main.humidity}%</div>
-                </div>
-
-                <div className={'block'}>
-                    <p>Pressure</p>
-                    <div>{weather.data.main.pressure}hPa</div>
-                </div>
-
-            </div>
-
-            <div className={'information-sun'}>
-                <div>
+                <div className={'information-main'}>
                     <div className={'block'}>
-                        <p>Sunrise</p>
-                        <div>{results?.sunrise}</div>
+                        <p>Temperature min</p>
+                        <div>{weather.data.main.temp_min}<sup>&deg;C</sup></div>
                     </div>
 
                     <div className={'block'}>
-                        <p>Sunset</p>
-                        <div>{results?.sunset}</div>
+                        <p>Temperature max</p>
+                        <div>{weather.data.main.temp_max}<sup>&deg;C</sup></div>
                     </div>
 
                     <div className={'block'}>
-                        <p>Day length</p>
-                        <div>{results?.day_length}</div>
+                        <p>Humidity</p>
+                        <div>{weather.data.main.humidity}%</div>
                     </div>
 
                     <div className={'block'}>
-                        <p>Visibility</p>
-                        <div>{weather.data.visibility}mi</div>
+                        <p>Pressure</p>
+                        <div>{weather.data.main.pressure}hPa</div>
+                    </div>
+
+                </div>
+
+                <div className={'information-sun'}>
+                    <div>
+                        <div className={'block'}>
+                            <p>Sunrise</p>
+                            <div>{results?.sunrise}</div>
+                        </div>
+
+                        <div className={'block'}>
+                            <p>Sunset</p>
+                            <div>{results?.sunset}</div>
+                        </div>
+
+                        <div className={'block'}>
+                            <p>Day length</p>
+                            <div>{results?.day_length}</div>
+                        </div>
+
+                        <div className={'block'}>
+                            <p>Visibility</p>
+                            <div>{weather.data.visibility}mi</div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={'information-clouds'}>
-                <div className={'block'}>
-                    <p>Clouds</p>
-                    <div>{weather.data.clouds.all}%</div>
-                </div>
+                <div className={'information-clouds'}>
+                    <div className={'block'}>
+                        <p>Clouds</p>
+                        <div>{weather.data.clouds.all}%</div>
+                    </div>
 
-                <div className={'block'}>
-                    <p>Wind speed</p>
-                    <div>{weather.data.wind.speed}km\h</div>
-                </div>
+                    <div className={'block'}>
+                        <p>Wind speed</p>
+                        <div>{weather.data.wind.speed}km\h</div>
+                    </div>
 
-                <div className={'block'}>
-                    <p>Degrees</p>
-                    <div>{weather.data.wind.deg}<sup>&deg;</sup></div>
-                </div>
+                    <div className={'block'}>
+                        <p>Degrees</p>
+                        <div>{weather.data.wind.deg}<sup>&deg;</sup></div>
+                    </div>
 
-                <div className={'block'}>
-                    <p>Wind gust</p>
-                    <div>{weather.data.wind.gust}km\h</div>
+                    <div className={'block'}>
+                        <p>Wind gust</p>
+                        <div>{weather.data.wind.gust}km\h</div>
+                    </div>
                 </div>
-            </div>
         </div>
     );
 };
